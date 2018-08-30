1d6ac26f1573e6bf504063b0e272b9f2d6018d9e
Don Wilson <rdwilson@ancestry.com>
2018-08-30 13:44:09 -0600

    Implement form validation on the account page

--- client/src/pages/AccountPage.js
+++ client/src/pages/AccountPage.js
@@ -7,24 +7,27 @@ class AccountPage extends Component {
 		bankUsername: 'MyUsername',
 		bankAccountId: 'MyAccountId',
 		bankSecretQuestions: [
-			{ question: 'What is your hair color?', answer: 'Fulvous' }
-		]
+			{ question: 'What is your hair color?', answer: 'Fulvous' },
+		],
+		password: '',
+		passwordConfirm: '',
+		passwordIsValid: true,
 	};
 
-	changeBank = (e) => {
+	onChangeBank = (e) => {
 		this.setState({
 			bank: e.target.value
 		});
 	};
 
-	changeTextField = (fieldName) => {
+	onChangeTextField = (fieldName) => {
 		return (e) => {
 			this.state[fieldName] = e.target.value;
 			this.setState(this.state);
 		};
 	};
 
-	addSecretQuestion = (e) => {
+	onAddSecretQuestion = (e) => {
 		e.preventDefault();
 		this.state.bankSecretQuestions.push({ question: '', answer: '' });
 		this.setState({
@@ -32,7 +35,7 @@ class AccountPage extends Component {
 		});
 	};
 
-	removeQuestion = (index) => {
+	onRemoveQuestion = (index) => {
 		return (e) => {
 			e.preventDefault();
 			this.state.bankSecretQuestions.splice(index, 1);
@@ -42,7 +45,7 @@ class AccountPage extends Component {
 		};
 	};
 
-	changeQuestion = (index, fieldName) => {
+	onChangeQuestion = (index, fieldName) => {
 		return (e) => {
 			this.state.bankSecretQuestions[index][fieldName] = e.target.value;
 			this.setState({
@@ -51,7 +54,25 @@ class AccountPage extends Component {
 		};
 	};
 
-	save() {
+	save = (e) => {
+		e.preventDefault();
+
+		const { password, passwordConfirm } = this.state;
+		let allowSave = true;
+		let passwordIsValid = true;
+
+		if (password !== passwordConfirm) {
+			allowSave = false;
+			passwordIsValid = false;
+		}
+
+		this.setState({
+			passwordIsValid
+		});
+
+		if (allowSave) {
+			// Save the stuff
+		}
 
 	}
 
@@ -67,13 +88,13 @@ class AccountPage extends Component {
 					<div className="form-group row">
 						<label htmlFor="inputEmail" className="col-sm-2 col-form-label">Email</label>
 						<div className="col-sm-3">
-							<input onChange={this.changeTextField('email')} type="email" className="form-control" id="inputEmail" placeholder="email" value={account.email} />
+							<input onChange={this.onChangeTextField('email')} type="email" className="form-control" id="inputEmail" placeholder="email" value={account.email} />
 						</div>
 					</div>
 					<div className="form-group row">
 						<label htmlFor="inputPassword" className="col-sm-2 col-form-label">Password</label>
 						<div className="col-sm-3">
-							<input type="password" className="form-control" id="inputPassword" placeholder="password" />
+							<input onChange={this.onChangeTextField('password')} value={account.password} type="password" className={`form-control ${account.passwordIsValid ? '' : 'is-invalid'}`} id="inputPassword" placeholder="password" />
 							<small className="form-text text-muted">
 								Passwords left blank will not be modified.
 							</small>
@@ -82,13 +103,16 @@ class AccountPage extends Component {
 					<div className="form-group row">
 						<label htmlFor="inputConfirmPassword" className="col-sm-2 col-form-label">Confirm password</label>
 						<div className="col-sm-3">
-							<input type="password" className="form-control" id="inputConfirmPassword" placeholder="password" />
+							<input onChange={this.onChangeTextField('passwordConfirm')} value={account.passwordConfirm} type="password" className={`form-control ${account.passwordIsValid ? '' : 'is-invalid'}`} id="inputConfirmPassword" placeholder="password" />
+							<div className="invalid-feedback">
+								Passwords don't match
+							</div>
 						</div>
 					</div>
 					<div className="form-group row">
 						<label htmlFor="selectBank" className="col-sm-2 col-form-label">Bank</label>
 						<div className="col-sm-3">
-							<select onChange={this.changeBank} className="form-control" id="selectBank" value={account.bank}>
+							<select onChange={this.onChangeBank} className="form-control" id="selectBank" value={account.bank}>
 								<option>choose&hellip;</option>
 								<option value="zions_bank">Zions Bank</option>
 								<option value="uccu">UCCU</option>
@@ -98,7 +122,7 @@ class AccountPage extends Component {
 					<div className="form-group row">
 						<label htmlFor="inputBankAccountId" className="col-sm-2 col-form-label">Bank account ID</label>
 						<div className="col-sm-3">
-							<input onChange={this.changeTextField('bankAccountId')} type="text" className="form-control" id="inputBankAccountId" placeholder="bank account id" value={account.bankAccountId} />
+							<input onChange={this.onChangeTextField('bankAccountId')} type="text" className="form-control" id="inputBankAccountId" placeholder="bank account id" value={account.bankAccountId} />
 						</div>
 					</div>
 					<div className="form-group row">
@@ -109,17 +133,17 @@ class AccountPage extends Component {
 								return (
 									<div key={index} className="row mb-3">
 										<div className="col-sm-4">
-											<input onChange={this.changeQuestion(index, 'question')} type="text" name={`question[${index}]`} value={qa.question} placeholder="Question" className="form-control" />
+											<input onChange={this.onChangeQuestion(index, 'question')} type="text" name={`question[${index}]`} value={qa.question} placeholder="Question" className="form-control" />
 										</div>
 										<div className="col-sm-3">
-											<input onChange={this.changeQuestion(index, 'answer')} type="text" name={`answer[${index}]`} value={qa.answer} placeholder="Answer" className="form-control" />
+											<input onChange={this.onChangeQuestion(index, 'answer')} type="text" name={`answer[${index}]`} value={qa.answer} placeholder="Answer" className="form-control" />
 										</div>
-										<button onClick={this.removeQuestion(index)} className="destroy btn btn-link" title="Remove this question and its answer"><span className="glyphicon glyphicon-trash"></span></button>
+										<button onClick={this.onRemoveQuestion(index)} className="destroy btn btn-link" title="Remove this question and its answer"><span className="glyphicon glyphicon-trash"></span></button>
 									</div>
 								);
 							})}
 
-							<button onClick={this.addSecretQuestion} type="button" className="btn btn-secondary"><span className="glyphicon glyphicon-plus"></span> New Question</button>
+							<button onClick={this.onAddSecretQuestion} type="button" className="btn btn-secondary"><span className="glyphicon glyphicon-plus"></span> New Question</button>
 						</div>
 
 
